c8715418efb5b13ac8a4afa5d07eacb5a5985d72
James Wilson <jamestwilson@live.com>
2018-05-06 19:45:44 -0700

    Finish grades up to 5/6

--- Irvine/ch05/64 bit/TestRandom.asm
+++ Irvine/ch05/64 bit/TestRandom.asm
@@ -1,54 +1,51 @@
-WriteInt64 PROTO
-ExitProcess PROTO
-Crlf PROTO
-.data
-MyArray QWORD 1d,2d,3d,4d,5d,6d
+;Fibonacci 
 
+ExitProcess	proto
+WriteInt64	proto
+Crlf		proto
+
+.data
+nMinus2 qword 0
+nMinus1 qword 1
+n		qword 0
+counter qword 47-1
 .code
 main proc
-	mov rcx, 6
-	mov rbx, 0
-	mov r12, 8
-L1:
-	mov rax, rbx
-	mul r12
-	mov r11, rax
-	mov rax, MyArray[r11]
-	call WriteInt64
-	inc rbx
-loop L1
 	
+	mov rcx, counter
+
+	mov rax, nMinus2
+	call WriteInt64 
 	call Crlf
 
-	mov rcx, 3
-	mov rbx, 0
-L2:
-	mov rax, rbx
-	mul r12
-	mov r11, rax					      ;r11 is first index offset
-	mov rbp, r11
-	add rbp, 8					        ;rbp is the second index offset
-
-	;swapping
-	mov r14, MyArray[r11]				;store the first value in r14
-	mov r15, MyArray[rbp]				;store the second value in r15
-	mov MyArray[r11], r15
-	mov MyArray[rbp], r14
-
-	add rbx, 2			
-loop L2
-	
-	mov rcx, 6
-	mov rbx, 0
-L3:
-	mov rax, rbx
-	mul r12
-	mov r11, rax
-	mov rax, MyArray[r11]
+	mov rax, nMinus1
 	call WriteInt64
-	inc rbx
-loop L3
-	call ExitProcess
+	call Crlf
+	call Fibonacci
+	
+	
+   call ExitProcess
 main endp
 
+Fibonacci proc
+myLoop:
+	mov r9, nMinus2
+	add r9, nMinus1
+		mov n,r9
+
+		
+	mov rax, n
+	call WriteInt64
+	call Crlf
+
+	mov r9, nMinus1
+	mov nMinus2, r9
+
+		
+	mov r9, n
+	
+	mov nMinus1, r9
+	loop myLoop
+ret
+Fibonacci endp
 end
--- Project32_VS2017/AddTwo.asm
+++ Project32_VS2017/AddTwo.asm
@@ -1,96 +1,42 @@
+;Random A screen
+
 include Irvine32.inc
-
 .data
-CaseTable BYTE 'A'			
-DWORD AndProcedure			
-EntrySize = ($ - CaseTable)
-BYTE 'B'
-DWORD OrProcedure
-BYTE 'C'
-DWORD NotProcedure
-BYTE 'D'
-DWORD XorProcedure
-
-NumberOfEntries = ($ - CaseTable) / EntrySize ; 
-prompt BYTE "Enter A for 'x AND y', B for 'x OR y', C for 'NOT x', D for 'x XOR y' other key to exit the program: ",0
-msgA BYTE "x AND y",0
-msgB BYTE "x OR y",0
-msgC BYTE "NOT x",0
-msgD BYTE "x XOR y",0
-hexVal DWORD ?
-andMsg BYTE "Enter two hexadecimal integers to AND them",0
-orMsg BYTE "Enter two hexadecimal integers to OR them",0
-notMsg BYTE "Enter a hexadecimal integer to NOT it",0
-xorMsg BYTE "Enter two hexadecimal integers to XOR them",0
+rows BYTE 0
+columns BYTE 0
+RandomRow BYTE 0
+RandomColumn BYTE 0
 
 .code
 main proc
 
-mov edx, offset prompt			
-call WriteString				
+call GetMaxXY 
+mov rows,al
+mov columns, dl
+mov ecx, 100
+
+myLoop:
+movzx eax,rows
+call RandomRange
+mov RandomRow,al
+
+movzx eax, columns
+call RandomRange
+mov RandomColumn, al
+
+mov dh,RandomRow 
+mov dl,RandomColumn 
+call Gotoxy
+
+mov al, 'A'			
+call WriteChar
 call crlf
 
-call ReadChar					
-mov ebx, offset CaseTable		
-mov ecx, NumberOfEntries		
+mov eax,100				
+call Delay
+	
+	loop myLoop
 
-L1:
-	cmp al, [ebx] ; comparison
-	jne L2 ; jump if no
-	call NEAR PTR [ebx + 1] 
-	jmp L3					; exit 
-
-L2:
-	add ebx, EntrySize		
-	loop L1					
-
-L3:
-	exit 
-
+invoke ExitProcess,0
 main endp
-
-AndProcedure proc
-mov edx, offset andMsg
-call WriteString		
-call crlf			
-
-call ReadHex			 
-mov hexVal, eax			
-call ReadHex		
-call crlf
-ret
-AndProcedure endp
-
-OrProcedure proc
-mov edx, offset orMSg
-
-call WriteString		
-call crlf
-ret
-OrProcedure endp
-
-NotProcedure proc
-mov edx, offset notMSg
-call WriteString		
-call crlf		
-
-call ReadHex			
-NOT eax					
-call WriteHex		
-call crlf
-ret
-NotProcedure endp
-
-XorProcedure proc
-mov edx, offset xorMsg
-call WriteString
-call crlf
-
-call ReadHex			
-mov hexVal, eax			
-call ReadHex		
-XOR eax, hexVal			
-call crlf
-ret
-XorProcedure endp
 end main
