82e70b4033621daea62ab1a926198bc0e08a70c6
James Wilson <jamestwilson@live.com>
2018-06-03 20:22:00 -0700

    update grades

--- Project32_VS2017/AddTwo.asm
+++ Project32_VS2017/AddTwo.asm
@@ -1,104 +1,95 @@
-ExitProcess PROTO
-Process_A PROTO
-Process_B PROTO
-Process_C PROTO
-Process_D PROTO
-Process_E PROTO
-ReadChar PROTO
-Crlf PROTO
+INCLUDE Irvine32.inc
+WriteInt64 PROTO
 WriteString PROTO
-
+ReadString PROTO
+ReadInt64 PROTO
+Crlf PROTO 
 .data
-CaseTable BYTE 'A' 
-DWORD Process_A 
-EntrySize = ($ - CaseTable)
-BYTE 'B'
-DWORD Process_B
-BYTE 'C'
-DWORD Process_C
-BYTE 'D'
-DWORD Process_D
-BYTE 'E'
-DWORD Process_E
+table BYTE '1'
+    DWORD PRINT AND
+EntrySize = ($-OprTable)
+    BYTE '2'
+    DWORD PRINT_OR
+    BYTE '3'
+    DWORD PRINT_NOT
+    BYTE '5'
+    DWORD PRINT_XOR
+    BYTE '5'
+    DWORD EXIT PROG
+CountEntries = ($-table) / EntrySize
+string1 BYTE "1. x and y", 0dh,0ah,0
+string2 BYTE "2. x or y", 0dh,0ah,0
+string3 BYTE "3. not x", 0dh,0ah,0
+string4 BYTE "4. x xor y", 0dh,0ah,0
+string5 BYTE "5. Exit", 0dh,0ah,0
+stringEnter BYTE "Enter choice: ",0
 
-NumberOfEntries = ($ - CaseTable) / EntrySize
-prompt BYTE "Press choose one of the following: ", 0
-optionA BYTE "A) ", 0
-optionB BYTE "B) ", 0
-optionC BYTE "C) ", 0
-optionD BYTE "D) ", 0
-optionE BYTE "E) ", 0
-msgA BYTE "ProcessA", 0
-msgB BYTE "ProcessB", 0
-msgC BYTE "ProcessC", 0
-msgD BYTE "ProcessD", 0
-msgE BYTE "ProcessE", 0
-
-
+stringAnd BYTE "AND operation",0
+stringOr BYTE "OR operation",0
+stringNot BYTE "NOT operation",0
+stringXor BYTE "XOR operation",0
+stringThank BYTE "Thank you ",0
 .code
-main proc
-	mov rdx,OFFSET prompt 
-	call WriteString
-	call Crlf
-	mov rdx,OFFSET optionA
-	call WriteString
-	call Crlf
-	mov rdx,OFFSET optionB
-	call WriteString
-	call Crlf
-	mov rdx,OFFSET optionC
-	call WriteString
-	call Crlf
-	mov rdx,OFFSET optionD 
-	call WriteString
-	call Crlf
-	mov rdx,OFFSET optionE 
-	call WriteString
-	call Crlf
-
-	call ReadChar 
-	mov rbx,OFFSET CaseTable 
-	mov rcx,NumberOfEntries 
+PROC
+        call Clrscr
+        mov edx, OFFSET string1
+        callWriteString
+        mov edx, OFFSET string2
+        call WriteString
+        mov edx, OFFSET string3
+        call WriteString
+        mov edx, OFFSET string4
+        call WriteString
+        mov edx, OFFSET string5
+        call WriteString
+        mov edx, OFFSET stringEnter
+        call WriteString
+        call ReadChar
+        call WriteChar
+        mov esi, OFFSET table
+        mov ecx,CountEntries
+        call Crlf
 
 L1:
-	cmp al,[rbx] 
-	jne L2 
-	call NEAR PTR [rbx + 1] 
-	call WriteString 
-	call Crlf
-	jmp L3 
-
+        cmp al,[esi]
+        jne L2
+        call NEAR PTR [esi + 1]
+        call Crlf
+        jmp L3
 L2:
-	add rbx,EntrySize ; point to the next entry
-	loop L1 
+        add esi,EntrySize
+        loop L1
 L3:
-	call exitProcess
+        exit
+main ENDP
 
+PRINT_AND PROC
+        mov edx, OFFSET stringAnd
+        call WriteString
+        ret
+PRINT_AND ENDP
 
-Process_A PROC
-	mov rdx,OFFSET msgA
-	ret
-Process_A ENDP
+PRINT_OR PROC
+        mov edx, OFFSET stringOr
+        call WriteString
+        ret
+PRINT_OR ENDP
 
-Process_B PROC
-	mov rdx,OFFSET msgB
-	ret
-Process_B ENDP
+PRINT_NOT PROC
+        mov edx, OFFSET stringNot
+        call WriteString
+        ret
+PRINT_NOT ENDP
 
-Process_C PROC
-	mov rdx,OFFSET msgC
-	ret
-Process_C ENDP
+PRINT_XOR PROC
+        mov edx, OFFSET stringXor
+        call WriteString
+        ret
+PRINT_XOR ENDP
 
-Process_D PROC
-	mov rdx,OFFSET msgD
-	ret
-Process_D ENDP
-
-Process_E PROC
-	mov rdx,OFFSET msgE
-	ret
-Process_E ENDP
-
-main endp
-end
+EXIT_PROG PROC
+        mov edx, OFFSET stringThank
+        call WriteString
+        ret
+        EXIT_PROG ENDP
+        END main 
--- Project64_VS2015/main.asm
+++ Project64_VS2015/main.asm
@@ -1,17 +1,60 @@
-; AddTwoSum_64.asm - Chapter 3 example.
-
-ExitProcess proto
+Exit process Proto
+Crlf       proto
+WriteInt64    proto
 .data
-sum qword 0
-.code
+my array QWORD 5, 1, 4, 2, 8
+
+.code 
 main proc
-   mov  eax,5
-   mov  ebx,6   
-   ;
-   mov  ecx,eax
-   mov  eax,ebx
-   mov  ebx,ecx
-   ;
-   call ExitProcess
+
+
+;print array
+
+mov rcx, LENGTHOF  myArray
+mov rsi, 0
+
+
+
+
+LOOOP:
+mov rax, myArray[rsi * 8] ;call write64 prints out the value in rax
+call WriteInt64
+inc rsi
+
+LOOP LOOOP
+call Crlf
+
+mov rcx,LENGTHOF myArray
+OUTERLOOP:
+	mov r15,rcx
+	mov rcx,LENGTHOF myArray
+	dec rcx
+	mov rsi, 0
+
+
+	INNERLOOP:
+		mov r9 , myArray[rsi * 8]
+		mov r11, rsi
+		inc r11
+		mov r10, myArray[r11 * 8]
+		cmp r9, r10
+		jle ENDLOOP ;if r9 is less than or equal to 10 it jumps
+
+
+	SWAP:
+			mov r13, r9
+			mov myArray[r11 * 8], r10
+			mov myArray[r11 * 8], r13
+		
+		END LOOP:
+		inc rsi
+	LOOP INNERLOOP
+	mov rcx,r15
+LOOP OUTERLOOP
+
+
+
+
+call ExitProcess
 main endp
-end
+endp
